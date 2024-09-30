e40b99d1a4263c48363219a2e7d36ebc6532371d
SaprankovaLE <saprankovale@mer.ci.nsu.ru>
2024-09-30 13:23:30 +0700

    Добавила лабораторные 1 и 2

---
+++ Lab1-2/Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            try
+            {
+                driver.Navigate().GoToUrl("https://www.example.com");
+                System.Threading.Thread.Sleep(5000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ Lab1-2/Lab2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PythonWebAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запуск веб-драйвера Chrome
+            IWebDriver driver = new ChromeDriver();
+
+            try
+            {
+                driver.Navigate().GoToUrl("https://www.python.org/");
+                Console.WriteLine("Открыта страница Python.");
+                System.Threading.Thread.Sleep(5000);
+
+                var downloadsButton = driver.FindElement(By.LinkText("Downloads"));
+                downloadsButton.Click();
+                Console.WriteLine("Перейдено на страницу Downloads.");
+                System.Threading.Thread.Sleep(5000);
+
+                var searchBox = driver.FindElement(By.Name("q"));
+                var searchButton = driver.FindElement(By.Id("submit"));
+                searchBox.SendKeys("python 3.11");
+                Console.WriteLine("Текст 'python 3.11' был введен в поле поиска.");
+                searchButton.Click();
+                Console.WriteLine("Нажата кнопка 'GO'.");
+                System.Threading.Thread.Sleep(5000);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                driver.Quit();
+                Console.WriteLine("Браузер закрыт.");
+            }
+        }
+    }
+}
